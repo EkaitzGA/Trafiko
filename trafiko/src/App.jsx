cfcdcc53c31169158c7a80b6f27aba4b75930ca3
EkaitzGA <ekaitzguerra@gmail.com>
2024-12-11 12:55:41 +0100

    elementos creados

--- trafiko/src/App.jsx
+++ trafiko/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import NavBar from './components/navbar/navbar'
+import Button from './components/button/button'
+import Footer from './components/footer/footer'
+import Form from './components/form/form'
 
 
 export default function App() {
     return (
         <div>
             <NavBar />
+            <Form />
+            <Button />
+            <Footer />
+
         </div>
     )
 }
---
+++ trafiko/src/components/footer/footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+        <div className="footer">
+            <p>© 2024 Trafiko. All rights reserved.</p>
+        </div>
+    )}
+
+    export default Footer
