be274fdb0a84803d9e6be129b50074b9359c3ea4
saulm96 <saulmorahernandez96@gmail.com>
2024-12-16 15:58:29 +0100

    Merge a dev para juntar casi todo

--- trafiko/src/components/incidences/Incidences.jsx
+++ trafiko/src/components/incidences/Incidences.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import {CleanedDataForIncidencesFromTheApiCall} from "./incidences-apiCall"
-import "./incidence-styles.css"
+import React, { useState, useEffect } from "react";
+import { CleanedDataForIncidencesFromTheApiCall } from "./incidences-apiCall";
+import "./incidence-styles.css";
 // Main Component to Fetch and Display Incidents
 // Main Component to Fetch and Display Incidents
 const TrafficIncidents = () => {
   const [incidents, setIncidents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     const fetchIncidentsData = async () => {
@@ -29,7 +29,7 @@ const TrafficIncidents = () => {
   };
 
   const filteredIncidents = incidents.filter((incident) => {
-    if (filter === '') return true;
+    if (filter === "") return true;
     return incident.incidenceType === filter;
   });
 
@@ -44,17 +44,39 @@ const TrafficIncidents = () => {
   return (
     <section className="incidences-container">
       <h1>Incidencias de Tráfico en Bizkaia</h1>
-      <div className="button-container">
-        <button className='boton-obras' onClick={() => handleFilterChange('Obras')}>Obras</button>
-        <button className='button-accidente' onClick={() => handleFilterChange('Accidente')}>Accidente</button>
-        <button className='button-seguridad-vial' onClick={() => handleFilterChange('Seguridad vial')}>Seguridad Vial</button>
-        <button className='button-otros' onClick={() => handleFilterChange('Otras incidencias')}>Otros</button>
-        <button className='button-todos' onClick={() => handleFilterChange('')}>Todos</button>
+      <div className="boton-container">
+        <button className="boton-todos" onClick={() => handleFilterChange("")}>
+          Todos
+        </button>
+        <button
+          className="boton-obras"
+          onClick={() => handleFilterChange("Obras")}
+        >
+          Obras
+        </button>
+        <button
+          className="boton-accidente"
+          onClick={() => handleFilterChange("Accidente")}
+        >
+          Accidente
+        </button>
+        <button
+          className="boton-seguridad-vial"
+          onClick={() => handleFilterChange("Seguridad vial")}
+        >
+          Seguridad Vial
+        </button>
+        <button
+          className="boton-otros"
+          onClick={() => handleFilterChange("Otras incidencias")}
+        >
+          Otros
+        </button>
       </div>
       {filteredIncidents.length === 0 ? (
         <p>No hay incidencias</p>
       ) : (
-        <div>
+        <div className="incident-card-container">
           {filteredIncidents.map((incident, index) => (
             <IncidentItem key={index} incident={incident} />
           ))}
@@ -67,7 +89,7 @@ const TrafficIncidents = () => {
 // Individual Incident Item Component
 const IncidentItem = ({ incident }) => {
   //Take thhe incidenceType of the incident and save it in a const
-  const className = incident.incidenceType.replace(/\s+/g, '-').toLowerCase(); 
+  const className = incident.incidenceType.replace(/\s+/g, "-").toLowerCase();
 
   return (
     <div className={`incident-card ${className}`}>
